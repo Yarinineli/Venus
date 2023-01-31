c1603ac9ff96dbe4d17aaf820f6f6ffdfc527d5b
Fatih Yarinineli <fatih.yarinineli@gmail.com>
2023-01-31 09:19:34 +0100

    tut

--- Beispiel 2.py
+++ Beispiel 2.py
@@ -1,3 +1,31 @@
-for numbers in range(0,10):
-    if numbers // 2:
-        print(numbers // 2)
+import time
+command = ""
+started = False
+stopped = False
+print("Welcome")
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("The Car is already started, fool")
+        else:
+            started = True
+            print("The Car started, brum brum")
+    elif command == "stop":
+        if stopped:
+            print("You're not movin', fool")
+        else:
+            stopped = True
+            print("The car has stopped")
+
+    elif command == "quit":
+        print("Thank you for the ride!")
+        time.sleep(1)
+        break
+    else:
+        print("""I dont understand, this are the following commands:
+        
+Start - Starting the car
+Stop - Stopping the car
+Quit - Quitting the Game""")
+
